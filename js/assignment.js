a5946a2d90363442dec55cb9be5254f67909a0cd
Tevi Placca <teviplacca@gmail.com>
2024-12-19 15:45:07 -0500

    modify js assigment and js index

--- js/assignment.js
+++ js/assignment.js
@@ -7,24 +7,34 @@ const resolutionElement = document.getElementById("resolution")
 const willMeetResolutionElement = document.getElementById("will-meet-resolution")
 const submissionBtn = document.getElementById("submission-btn")
 
-// Declare your variables here.
+// Declare a constant variable here.
+const resolution = "read more";
+const currentYear = 2025;
+let willmeetResolution = true;
 
-function updateYear() {
-    // write the logic
-
+function updateYear () {
+    // Update this function
+    yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
-    // Update this function 
+    // update this function
+    resolutionElement.innerText=resolution;
+}
 
+function updateWillmeetResolution() {
+    // update this function
+    willMeetResolutionElement.innerText = willMeetResolution
 }
 
 function render() {
-    // Finish writing this function
-
+    // update this function
+    updateYear();
+    updateResolution();
+    updatewillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
-    // Finish writing this function
-
-})
+    // update this function
+    render();
+});
--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@
 // that are not declared
 "use strict"
 
+// Do the experiment section 
+// Select a button element 
+//const testButton = document.getElementById("test-button"); 
+// Add an event listener to the button
+ //testButton.addEventListener("click", function() { 
+    //alert("Button was clicked!"); 
+//}); 
+// Experiment with variables
+ //let testVariable = "Hello, World!"; 
+ //console.log(testVariable);
